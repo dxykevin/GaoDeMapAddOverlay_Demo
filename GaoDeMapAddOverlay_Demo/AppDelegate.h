5cedf0a847df2889c9309ec8cf52b8b7af221e1e
kevin <duxinyuan@avori.cn>
2016-06-13 17:18:16 +0800

    Initial Commit

---
+++ GaoDeMapAddOverlay_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GaoDeMapAddOverlay_Demo
+//
+//  Created by dxykevin on 16/6/13.
+//  Copyright © 2016年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
